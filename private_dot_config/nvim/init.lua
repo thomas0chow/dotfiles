08b23bb7e9a2c1bfdd5527bd7e30abeffd5b2010
thomas.chow <thomas0chow@gmail.com>
2024-10-13 23:33:32 +0900

    Set en_use in nvim

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -14,3 +14,6 @@ end
 -- Formatter
 -- vim.cmd [[autocmd BufWrite *.py call CocAction('format')]]
 vim.cmd [[autocmd BufWritePre * :call CocAction('runCommand', 'editor.action.organizeImport')]]
+
+-- Language
+vim.cmd("language en_US")
