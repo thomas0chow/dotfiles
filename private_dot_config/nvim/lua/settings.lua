b223ea08b4717750bc612b6f4d1ec4705f3ef708
thomas.chow <thomas0chow@gmail.com>
2025-01-23 17:00:34 +0900

    Add settings of nvim-ufo

--- private_dot_config/nvim/lua/settings.lua
+++ private_dot_config/nvim/lua/settings.lua
@@ -72,9 +72,12 @@ vim.o.foldcolumn = '1' -- '0' is not bad
 vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
+vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
 
 keyset('n', 'zR', require('ufo').openAllFolds)
 keyset('n', 'zM', require('ufo').closeAllFolds)
+keyset('n', 'zr', require('ufo').openFoldsExceptKinds)
+keyset('n', 'zm', require('ufo').closeFoldsWith) -- closeAllFolds == closeFoldsWith(0)
 
 -- neo-tree
 keyset("n", "<leader>bb", ":Neotree toggle<CR>", { silent = true })
